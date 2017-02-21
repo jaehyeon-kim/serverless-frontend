fddd5fa4fefcb2057a1b04ddec660b2b84fe116f
jaehyeon-kim <dottami@gmail.com>
2017-02-22 05:10:17 +1100

    extra updates

--- serverless-spa/learnjs-trial/public/app.js
+++ serverless-spa/learnjs-trial/public/app.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
 var learnjs = {};
 
 learnjs.problemView = function(problemNumber) {
-    var title = 'Problem #' + problemNumber + ' Coming soon!';
-    return $('<div class="problem-view">').text(title);
+  var view = $('.templates .problem-view').clone();
+  view.find('.title').text('Problem #' + problemNumber);
+  return view;
 }
 
 learnjs.showView = function(hash) {
-    var routes = {
-        '#problem': learnjs.problemView
-    };
-    var hashParts = hash.split('-');
-    var viewFn = routes[hashParts[0]];
-    if (viewFn) {
-        $('.view-container').empty().append(viewFn(hashParts[1]));
-    }
+  var routes = {
+    '#problem': learnjs.problemView
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
 }
 
 learnjs.appOnReady = function() {
-    window.onhashchange = function() {
-        learnjs.showView(window.location.hash);
-    };
-
+  window.onhashchange = function() {
     learnjs.showView(window.location.hash);
+  };
+  learnjs.showView(window.location.hash);
 }
