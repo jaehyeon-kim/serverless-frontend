e6a7ae3b0a907d52752f5172705d10efe2eda166
jaehyeon-kim <dottami@gmail.com>
2017-03-17 14:12:51 +1100

    react-router not complete yet

--- mindspace-react/reactjs-basics-trials/src/app/components/Header.js
+++ mindspace-react/reactjs-basics-trials/src/app/components/Header.js
@@ -6,7 +6,8 @@ export const Header = (props) => {
             <div className="container">
                 <div className="navbar-header">
                     <ul className="nav navbar-nav">
-                        <li><a href="#">{props.homeLink}</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">User</a></li>
                     </ul>
                 </div>
             </div>
--- mindspace-react/reactjs-basics-trials/src/app/components/Home.js
+++ mindspace-react/reactjs-basics-trials/src/app/components/Home.js
@@ -1,92 +1,14 @@
 import React from "react";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            status: 0,
-            homeLink: props.initialLinkName
-        };
-        setTimeout(() => {
-            this.setState({
-                status: 1
-            });
-        }, 3000);
-        console.log("Constructor");
-    }
-
-    componentWillMount() {
-        console.log("Component will mount");
-    }
-
-    componentDidMount() {
-        console.log("Component did mount");
-    }
-
-    componentWillreceiveProps(nextProps) {
-        console.log("Component will receive props", nextProps);
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("Should component update", nextProps, nextState);
-        // if (nextState.status == 1) {
-        //     return false;
-        // }
-        return true;
-    }
-
-    componentWillupdate(nextProps, nextState) {
-        console.log("Component will update", nextProps, nextState);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("Component did update", prevProps, prevState);
-    }
-
-    componentWillUnmount() {
-        console.log("Component will unmount");
-    }
-    
-    onMakeOlder() {
-        this.setState({
-            age: this.state.age + 3
-        });
-    }
-
-    onChangeLink() {
-        this.props.changeLink(this.state.homeLink);
-    }
-
-    onHandleChange(event) {
-        this.setState({
-            homeLink: event.target.value
-        }); 
-    }
-
     render() {
         return (
             <div>
-                <p>In a new Component!</p>
-                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
-                <p>Status: {this.state.status}</p>
-                <ht/>
-                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older</button>
-                <hr/>
-                <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
-                <hr/>
-                <input type="text" value={this.state.homeLink} onChange={(event) => this.onHandleChange(event)}/>
-                <button onClick={() => this.onChangeLink()} className="btn btn-primary">Change Header Link</button>
+                <h3>Home</h3>
             </div>
         );
     }
 }
 
-//this.onMakeOlder.bind(this)
 
-Home.PropTypes = {
-    name: React.PropTypes.string,
-    initialAge: React.PropTypes.number,
-    greet: React.PropTypes.func,
-    initialLinkName: React.PropTypes.string
-}
+
