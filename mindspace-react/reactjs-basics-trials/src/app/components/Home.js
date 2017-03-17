e707dfd9c35ecc13739a14f62051ddc88723f58a
jaehyeon-kim <dottami@gmail.com>
2017-03-17 11:51:57 +1100

    stateless components

--- mindspace-react/reactjs-basics-trials/src/app/components/Home.js
+++ mindspace-react/reactjs-basics-trials/src/app/components/Home.js
@@ -4,8 +4,14 @@ export class Home extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            age: props.initialAge
+            age: props.initialAge,
+            status: 0
         };
+        setTimeout(() => {
+            this.setState({
+                status: 1
+            });
+        }, 3000);
     }
     
     onMakeOlder() {
@@ -19,6 +25,7 @@ export class Home extends React.Component {
             <div>
                 <p>In a new Component!</p>
                 <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
                 <ht/>
                 <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older</button>
             </div>
