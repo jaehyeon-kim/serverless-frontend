385c585de4315f1ab8f68d5789410aea42659fec
jaehyeon-kim <dottami@gmail.com>
2017-03-17 11:54:09 +1100

    to stateless components

---
+++ mindspace-react/reactjs-basics-trials/archive/10_stateless_components/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header homeLink="Home"/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home name={"Bernie"} initialAge={30} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
+
+
