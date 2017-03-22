6617de68760838f2684c1b5fb0b368322a3d0f28
jaehyeon-kim <dottami@gmail.com>
2017-03-22 21:45:17 +1100

    ch06 section 3 to revisit

---
+++ react-quickly/ch06/other-libraries-jquery/src/app/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!!!!</h1>
+            </div>
+        );
+    }
+}
